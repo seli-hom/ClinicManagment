8d10356675fc185627c1580651a9bbe28a4fe904
Talia Muro <2050123@edu.vaniercollege.qc.ca>
2025-04-08 12:24:30 -0400

    Added instance variables for Appointment class

--- ClinicManagement/src/main/java/org/example/Appointment.java
+++ ClinicManagement/src/main/java/org/example/Appointment.java
@@ -1,4 +1,11 @@
 package org.example;
 
+import java.sql.Time;
+import java.util.Date;
+
 public class Appointment {
+    private Patient patient;
+    private Doctor doctor;
+    private Date date;
+    private Time time;
 }
