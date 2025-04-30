30768bf5dd51e22a767531011533e035095ff21c
seli-hom <efremselihom1@gmail.com>
2025-04-30 10:55:34 -0400

    you can drive

--- ClinicManagement/src/main/java/org/example/ClinicController.java
+++ ClinicManagement/src/main/java/org/example/ClinicController.java
@@ -1,9 +1,25 @@
 package org.example;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 public class ClinicController {
     private ClinicManagementGUI view;
     private SystemManager model;
 
+    public ClinicController(ClinicManagementGUI view, SystemManager model) {
+        this.view = view;
+        this.model = model;
 
+        class AddPatientListener implements ActionListener {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                
+            }
+        }
+
+
+    }
 
 }
