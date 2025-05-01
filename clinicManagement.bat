5fa7189de1f4946217d8a9464bd30e408029e933
seli-hom <efremselihom1@gmail.com>
2025-05-01 17:54:28 -0400

    no work :(

---
+++ clinicManagement.bat
@@ -0,0 +1,3 @@
+@echo off
+java --module-path "C:\Users\Samuel\JavaFxGUI\javafx-sdk-23.0.1\lib" --add-modules javafx.controls,javafx.fxml -jar "C:\Users\Samuel\Selihom\Desktop\ClinicManagement.jar"
+pause
